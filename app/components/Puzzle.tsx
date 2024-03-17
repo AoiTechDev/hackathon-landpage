f8d5d75aa82971c52dfc3d8fbf72b40ec07987a3
Pawel <pawel.bornikowski@gmail.com>
2024-03-17 07:08:39 +0100

    feat: add 2nd section

---
+++ app/components/Puzzle.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import React from "react";
+import puzzle from "./svg/puzzle.svg";
+import mouse from "./svg/mouse.svg";
+const Puzzle = () => {
+  return (
+    <div className="w-full flex justify-between h-screen items-center pl-16 pr-40">
+      <div className=" w-[730px] space-y-10">
+        <h2 className="text-[39px] ">
+          {" "}
+          ClearFlow is your <span className="text-green">game-changer</span> when it comes to online
+          shopping
+        </h2>
+        <p className="font-light text-balance">
+          Make smarter, eco-friendly choices and ditch the manipulative tactics
+          with our user-friendly plugin
+        </p>
+      </div>
+      <div className="relative  z-0">
+        <Image src={puzzle} alt="" />
+        <Image src={mouse} alt="" className="absolute bottom-[3px] -right-[10px]" />
+      </div>
+    </div>
+  );
+};
+
+export default Puzzle;
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
-import line from './components/svg/line.svg'
+
 import union from './components/svg/union.svg'
+import Puzzle from "./components/Puzzle";
 export default function Home() {
   return (
     <div className="w-full   text-white relative min-h-screen">
       <Hero />
+      
       <span className="w-full block h-[1px] bg-green"></span>
-      <Image alt="" className="absolute  top-60 left-20 w-full"  src={union}/>
+      <Puzzle />
+      <Image alt="" className="absolute  top-40 left-20 w-full"  src={union}/>
     </div> 
   );
 }
